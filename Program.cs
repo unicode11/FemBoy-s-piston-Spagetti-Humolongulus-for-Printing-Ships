16bf7943dba87e7b6650d3e6a84ab7ba84797dfa
unicode11 <egoraleshin2006@gmail.com>
2023-04-27 01:28:10 +0300

    UPD1 - Пересчёт пистоноф

--- Program.cs
+++ Program.cs
@@ -23,15 +23,33 @@ namespace Script1
 {
     public sealed class Program : MyGridProgram
     {
+        // У сварщиков каждые 60 тиков проверяем условие "Работает(сваривает)"
+        //      Нет -> двигаем пистон
+        //      Да -> останавливаем(или не двигаем) пистон
+        // Как только первый пистон достигнет лимита -> активируется следующий
+        // Как только у проектора закончатса блоки на сварку -> он отключается и вот тебе ебать корабль
+
+
+        //
+        //
         // SCRIPT START BELOVE
 
+        //;
 
         public void Main(string args)
         {
-
+            var NUM = 1;
+            Sandbox.ModAPI.Ingame.IMyBlockGroup LIST = GridTerminalSystem.GetBlockGroupWithName("MYPistonGroup");
+            List<Sandbox.ModAPI.Ingame.IMyPistonBase> COCK = new List<Sandbox.ModAPI.Ingame.IMyPistonBase>();
+            LIST.GetBlocksOfType(COCK);
+            foreach (Sandbox.ModAPI.Ingame.IMyPistonBase BLYAT in COCK)
+            {
+                BLYAT.CustomName = ($"PISTON {NUM}");
+                NUM++; 
             }
         }
     }
+}
 
 
 // SCRIPT START ABOVE
