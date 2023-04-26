4fea1394e91405fe2b34c4f4af84f3c9562b031f
unicode11 <egoraleshin2006@gmail.com>
2023-04-26 23:50:46 +0300

    НАЧАЛО РАБОТЫ

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using VRageMath;
+using VRage.Game;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Utils;
+using Sandbox.ModAPI;
+using Sandbox.Game.Gui;
+using System.Runtime.CompilerServices;
+using VRage.Library.Utils;
+
+namespace Script1
+{
+    public sealed class Program : MyGridProgram
+    {
+        // SCRIPT START BELOVE
+
+
+        public void Main(string args)
+        {
+
+            }
+        }
+    }
+
+
+// SCRIPT START ABOVE
